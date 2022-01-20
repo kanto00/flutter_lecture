a0235b25fa6cc3f4dace9ed3e95479d59e118a57
YeHyoung Lee <kanto00@naver.com>
2022-01-20 21:05:17 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Snack Bar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack bar'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            print('Text Button is pressed.');
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Hello World !'),
+              ),
+            );
+          },
+          child: Text(
+            'Show me',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Colors.redAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
