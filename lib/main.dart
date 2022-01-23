d61896768ca4a55f2432678c34b694cf06d17f31
YeHyoung Lee <kanto00@naver.com>
2022-01-23 22:07:21 +0900

    5th commit, Navigator and pushNamed(otherfiles)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'ScreenA.dart';
+import 'ScreenB.dart';
+import 'ScreenC.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,53 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Myapp',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: FirstPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ScreenA(),
+        '/b': (context) => ScreenB(),
+        '/c': (context) => ScreenC()
+      },
     );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('First Page'),
-        ),
-        body: Center(
-          child: TextButton(
-            child: Text('Go to the Second Page'),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SecondPage()));
-            },
-          ),
-        ));
-  }
-}
-
-//  route는 반드시 material widget 아레에 child 로 생성되어야 한다.
-// 따라서 Second Page는 Material.push로 fisrt page 위에 쌓여져야함
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Second Page'),
-        ),
-        body: Center(
-          child: TextButton(
-            child: Text('Go to the First Page'),
-            onPressed: () {
-              Navigator.pop(ctx); // 가장 최근 저장된 것을 삭제
-            },
-          ),
-        ));
   }
 }
